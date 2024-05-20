b2b240c6d494cc8a0b4020eecbb1394ddc5623b7
atal98 <80337520+atal98@users.noreply.github.com>
2024-05-20 20:51:19 +0530

    Login and Register form created with auth

---
+++ src/components/Form.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import api from "../api";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
+
+const Form = ({ route, method }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const name = method === "login" ? "Login" : "Register";
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    try {
+      const res = await api.post(route, { username, password });
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      // Display error message (400 Bad Request)
+      if (error.response && error.response.status === 400) {
+        alert("400 Bad Request: Invalid username or password");
+      } else {
+        alert("An unexpected error occurred");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h1>{name}</h1>
+      <input
+        className="form-input"
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+
+      <input
+        className="form-input"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+
+      <button className="form-button" type="submit">
+        {name}
+      </button>
+    </form>
+  );
+};
+
+export default Form;
